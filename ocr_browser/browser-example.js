29cd727e4995bebcea6202ba5095b9d7207e51d8
khiemnv <khiemnv@hotmail.com>
2026-02-14 00:00:22 +0900

    new GUI: paste, drag

--- ocr_browser/browser-example.js
+++ ocr_browser/browser-example.js
@@ -33,10 +33,10 @@ async function main() {
   g_ocr = await Ocr.create({
     isDebug: true,
     models: {
-      detectionPath: "/assets/ch_PP-OCRv4_det_infer.onnx",
+      detectionPath: "./assets/ch_PP-OCRv4_det_infer.onnx",
       // recognitionPath: "/assets/ch_PP-OCRv4_rec_infer.onnx",
-      recognitionPath: "/assets/japan_PP-OCRv3_rec_infer.onnx",
-      dictionaryPath: "/assets/japan_dict.txt",
+      recognitionPath: "./assets/japan_PP-OCRv3_rec_infer.onnx",
+      dictionaryPath: "./assets/japan_dict.txt",
     },
     onnxOptions: g_onnxOptions,
   });
@@ -44,7 +44,7 @@ async function main() {
   // document.querySelector(".hide").style.visibility = "visible";
   document.querySelector("#title").textContent = "OCR is ready";
 
-  createApp(runOcr);
+  // createApp(runOcr);
 }
 
 async function runOcr({ imageUrl }) {
@@ -53,7 +53,8 @@ async function runOcr({ imageUrl }) {
   const duration = new Date().valueOf() - startTime;
 
   return {
-    text: result.map((v) => `${v.mean.toFixed(2)} ${v.text}`).join("\n"),
+    text: result.map((v) => `${v.text}`).join("\n"),
+    raw: result.map((v) => `${v.mean.toFixed(2)} ${v.text}`).join("\n"),
     duration,
   };
 }
@@ -61,30 +62,25 @@ async function runOcr({ imageUrl }) {
   const performanceEl = document.querySelector("#performance");
   const resultImageEl = document.querySelector("#result-image");
   const inputImageEl = document.querySelector("#input-image");
-function createApp(runOcr) {
-  inputImageEl.addEventListener("change", async (event) => {
-    const file = event.target.files?.[0];
-    if (!file) {
-      return;
-    }
-    const imageUrl = URL.createObjectURL(file);
-    await handleChange(imageUrl);
-  });
-  const handleChange = async (imageUrl) => {
-    // document.querySelectorAll("canvas").forEach((el) => el.remove());
-    resultTextEl.textContent = "Working in progress...";
-    resultImageEl.setAttribute("src", imageUrl);
-    const { text, duration } = await runOcr({ imageUrl });
-    resultTextEl.textContent = text;
-    performanceEl.textContent = `Performance: ${duration}ms (Close Chrome DevTools to get accureate result)`;
-  };
-}
+// function createApp(runOcr) {
+//   inputImageEl.addEventListener("change", async (event) => {
+//     const file = event.target.files?.[0];
+//     if (!file) {
+//       return;
+//     }
+//     const imageUrl = URL.createObjectURL(file);
+//     await handleChange(imageUrl);
+//   });
+
+//   const handleChange = async (imageUrl) => {
+//     // document.querySelectorAll("canvas").forEach((el) => el.remove());
+//     resultTextEl.textContent = "Working in progress...";
+//     resultImageEl.setAttribute("src", imageUrl);
+//     const { text, duration } = await runOcr({ imageUrl });
+//     resultTextEl.textContent = text;
+//     performanceEl.textContent = `Performance: ${duration}ms (Close Chrome DevTools to get accureate result)`;
+//   };
+// }
 
 main();
-document.getElementById("process_btn").onclick = async (event) => {
-  event.preventDefault();
-  var imageUrl = document.querySelector("#result-image").src;
-  const { text, duration } = await runOcr({ imageUrl });
-  console.log(text, duration);
-  resultTextEl.textContent = text
-};
+
